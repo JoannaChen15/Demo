850a89eb416778061384054eccfd7e43fe8695c3
Joanna <joufengchen15.gmail.com>
2023-04-18 16:26:59 +0800

    1.安裝SnapKit 2.makeConstraints小練習

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myView = UIView()
+        myView.backgroundColor = .gray
+        view.addSubview(myView)
+        
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        myView.addSubview(label)
+        
+        myView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(200)
+        }
+        
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.8)
+            make.width.equalToSuperview().multipliedBy(0.5)
+        }
+        
     }
 
 
