99df1a8584a2cf2234d007a9269f28fcc0652caf
elijah <a110482@yahoo.com.tw>
2023-04-19 11:19:57 +0800

    present 兩種頁面

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,9 +10,51 @@ import SnapKit
 
 
 class ViewController: UIViewController {
+    private let stackView = UIStackView()
+    private let redButton = UIButton()
+    private let blueButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(stackView)
+        stackView.distribution = .fillEqually
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
+        stackView.addArrangedSubview(redButton)
+        stackView.addArrangedSubview(blueButton)
+        redButton.backgroundColor = .red
+        blueButton.backgroundColor = .blue
+        redButton.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
+        blueButton.addTarget(self, action: #selector(buttonTapped(sender:)), for: .touchUpInside)
+    }
+    
+    @objc private func buttonTapped(sender: UIButton) {
+        let vc: UIViewController
+        if sender === redButton {
+            vc = RedViewController()
+        } else {
+            vc = BlueViewController()
+        }
+        present(vc, animated: true)
     }
 }
+
+// MARK: -
+class RedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+
+// MARK: -
+class BlueViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
