20c8b49b4707abcf5d0e4b2df2a873cd3d754723
Joanna <joufengchen15.gmail.com>
2023-04-19 09:06:32 +0800

    clean

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -14,33 +14,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myView = UIView()
-        myView.backgroundColor = .lightGray
-        view.addSubview(myView)
-        
-        myView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalToSuperview().offset(100)
-            make.width.equalToSuperview()
-            make.height.equalTo(200)
-        }
-        
-        let storyPageText = StoryPage.text
-        let label = UILabel()
-        label.text = storyPageText
-        label.textAlignment = .center
-        myView.addSubview(label)
-        
-        
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.8)
-            make.width.equalToSuperview().multipliedBy(0.5)
-        }
-        
-    }
-
-
 }
 
