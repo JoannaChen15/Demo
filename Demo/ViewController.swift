9d240cd0d97c9e3a9f46075f270d8c00332100b4
elijah <a110482@yahoo.com.tw>
2023-04-26 10:02:01 +0800

    建立按鈕的 title

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 import SnapKit
 
-
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController {    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+}
+
+
+struct ButtonDatas {
+    let titles = [
+        ["7", "8" ,"9", "÷"],
+        ["4", "5", "6", "x"],
+        ["1", "2", "3", "-"],
+        ["AC", "0", "=", "+"]
+    ]
 }
