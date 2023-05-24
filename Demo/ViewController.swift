9630996a1e1cb7d31ff8a412d7355edccc4b945a
Joanna <joufengchen15.gmail.com>
2023-05-24 18:01:57 +0800

    done

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -10,7 +10,6 @@ import SnapKit
 
 
 class ViewController: UIViewController {
-    let cities = City.data
     let cityPickView = UIPickerView()
     let label = UILabel()
 
@@ -20,16 +19,15 @@ class ViewController: UIViewController {
         
         view.addSubview(cityPickView)
         cityPickView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
+            make.top.equalToSuperview().inset(50)
             make.centerX.equalToSuperview()
-            make.height.equalTo(500)
-            make.width.equalTo(300)
         }
         
         cityPickView.delegate = self
         cityPickView.dataSource = self
         
-        label.text = "test"
+        label.text = "台北市中正區"
+        label.font = .systemFont(ofSize: 26)
         view.addSubview(label)
         label.snp.makeConstraints { make in
             make.top.equalTo(cityPickView.snp.bottom).offset(20)
@@ -47,6 +45,18 @@ extension ViewController: UIPickerViewDelegate {
             return cities[cityRow].districts[row].name
         }
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            if component == 0 {
+                pickerView.reloadComponent(1)
+                pickerView.selectRow(0, inComponent: 1, animated: true)
+                let districtsRow = pickerView.selectedRow(inComponent: 1)
+                label.text = "\(cities[row].name)\(cities[row].districts[districtsRow].name)"
+            } else {
+                let cityRow = pickerView.selectedRow(inComponent: 0)
+                label.text = "\(cities[cityRow].name)\(cities[cityRow].districts[row].name)"
+            }
+    }
 }
 
 extension ViewController: UIPickerViewDataSource {
@@ -60,7 +70,6 @@ extension ViewController: UIPickerViewDataSource {
         } else {
             let cityRow = pickerView.selectedRow(inComponent: 0)
             return cities[cityRow].districts.count
-            #warning("index out of range")
         }
     }
 }
@@ -74,18 +83,40 @@ struct District: Decodable {
     let zip: String
     let name: String
 }
+
+let cities: [City] = [
+    City(name: "台北市", districts: [
+        District(zip: "100", name: "中正區"),
+        District(zip: "103", name: "大同區"),
+        District(zip: "104", name: "中山區"),
+        District(zip: "110", name: "信義區"),
+        District(zip: "111", name: "松山區"),
+        // 添加更多台北市行政區...
+    ]),
+    City(name: "新北市", districts: [
+        District(zip: "200", name: "板橋區"),
+        District(zip: "220", name: "新莊區"),
+        District(zip: "221", name: "中和區"),
+        District(zip: "223", name: "三峽區"),
+        District(zip: "231", name: "新店區"),
+        // 添加更多新北市行政區...
+    ]),
+    City(name: "桃園市", districts: [
+        District(zip: "320", name: "中壢區"),
+        District(zip: "324", name: "平鎮區"),
+        District(zip: "325", name: "龍潭區"),
+        District(zip: "330", name: "桃園區"),
+        District(zip: "333", name: "八德區"),
+        // 添加更多桃園市行政區...
+    ])
+]
+
 //讀取 asset 裡的 JSON，將轉換後的 Decodable 型別資料存在 computed variable
-extension City {
-    static var data: [Self] {
-        var districtData = [Self]()
-        if let data = NSDataAsset(name: "taiwanDistricts")?.data {
-            do {
-                districtData = try JSONDecoder().decode([Self].self, from: data)
-            } catch {
-                print(error)
-            }
-            
-        }
-        return districtData
-    }
-}
+//extension City {
+//    static var data: [Self] {
+//        guard let data = NSDataAsset(name: "taiwanDistricts")?.data else {
+//            return []
+//        }
+//        return (try? JSONDecoder().decode([Self].self, from: data)) ?? []
+//    }
+//}
