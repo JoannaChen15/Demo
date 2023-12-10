f0b9a1911234d7468111167f5f0676b086a4a632
陳柔夆 <joufengchen@tanpeichengdeMacBook-Pro.local>
2023-12-10 17:08:56 +0800

    class struct mutating inout

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,30 +9,68 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        func filterFunc (oldArray:[Int], function: (Int) -> Bool) -> [Int]? {
-            var result: [Int]?
-            
-            for i in oldArray {
-                if function(i) {
-                    if result == nil {
-                        result = [Int]()
-                    }
-                result?.append(i)
-                }
-            }
-            return result
-        }
+        let letStruct = MyStruct(value: 10)
+        letStruct.modifyValue(newVal: 20)
+        letStruct.value = 20
         
-        let newArray = filterFunc(oldArray: numbers) { i in
-            return i > 5
-        }
+        var varStruct = MyStruct(value: 10)
+        varStruct.modifyValue(newVal: 20)
         
-        print(newArray ?? [])
+        modifyStruct(obj: varStruct)
+        
+        let letClass = MyClass(value: 10)
+        letClass.modifyValue(newVal: 20)
+        letClass.value = 20
+        
+        var varClass = MyClass(value: 20)
+        
+        modifyClass(obj: varClass)
+    }
+    
+    func modifyStruct(obj: MyStruct) {
+        obj.value = 100
+    }
+    
+    func modifyClass(obj: MyClass) {
+        obj.value = 100
+    }
+}
+
+struct MyStruct {
+    var value = 42
+
+    // 在value type的方法中直接修改属性，需要 mutating 关键字
+    mutating func modifyValue(newVal: Int) {
+        value = newVal
+    }
+}
+
+class MyClass {
+    var value = 42
+
+    // 在类的方法中直接修改属性，不需要 mutating 关键字
+    func modifyValue(newVal: Int) {
+        value = newVal
+    }
+    //
+    init(value: Int = 42) {
+        self.value = value
+    }
+}
+//函數中struct當參數且會改變本身的值時，需要加inout
+//呼叫函數時，參數前加&
+class demo {
+    func add_5(number: inout Int) -> Int {
+        number += 5
+        return number
+    }
+    
+    func test() {
+        var i = 3
+        var result = add_5(number: &i)
     }
 }
